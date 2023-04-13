d92716a42f00cba896b3c85c98f3eb6b3fad4ef7
Camila Gallegos <camigm1@gmail.com>
2023-04-13 17:00:43 -0600

    changes

---
+++ script.js
@@ -0,0 +1,22 @@
+const card = document.querySelector(".card");
+const myLibrary = [{ author: "Brandon", title: "elantris", pages: 213 }];
+
+function Book(title, author, pages) {
+  (this.title = title), (this.author = author), (this.pages = pages);
+}
+
+function addBookToLibrary() {}
+
+function display(object) {
+  const title = document.createElement("h3");
+  const author = document.createElement("h4");
+  const pages = document.createElement("h4");
+  title.innerHTML = object[0].title;
+  author.textContent = object[0].author;
+  pages.textContent = object[0].pages;
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+}
+
+display(myLibrary);
